cae62435655cb5d5dbec54528ab2cf0b8bb71ebd
fangweigao <fangweigao@creditease.cn>
2017-12-07 14:23:05 +0800

    API工程单独放置一个目录

--- monitor-web/src/main/java/com/creditease/monitor/response/ResponseCode.java
+++
@@ -1,8 +0,0 @@
-package com.creditease.monitor.response;
-
-import com.creditease.response.ResultMessage;
-
-public class ResponseCode {
-    @ResultMessage("测试{0},{1}")
-    public static final int errorCode = 20001;
-}
