e2ca8d767ffe0e1034e709ffadb59440c217486b
fangweigao <fangweigao@creditease.cn>
2017-12-18 17:05:29 +0800

    监控前端ETCD的封装

---
+++ monitor-console/monitor-web/src/main/java/com/creditease/monitor/etcd/entity/MonitorNoteDataEntity.java
@@ -0,0 +1,56 @@
+package com.creditease.monitor.etcd.entity;
+
+import java.util.List;
+
+public class MonitorNoteDataEntity {
+    //类型
+    private String type;
+    //日志路径
+    private List<String> path;
+    //IP地址
+    private List<String> ipAddress;
+    //切割规则存储
+    private String cleanRule;
+
+    private List<String> tomcatHostPort;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getPath() {
+        return path;
+    }
+
+    public void setPath(List<String> path) {
+        this.path = path;
+    }
+
+    public List<String> getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(List<String> ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getCleanRule() {
+        return cleanRule;
+    }
+
+    public void setCleanRule(String cleanRule) {
+        this.cleanRule = cleanRule;
+    }
+
+    public List<String> getTomcatHostPort() {
+        return tomcatHostPort;
+    }
+
+    public void setTomcatHostPort(List<String> tomcatHostPort) {
+        this.tomcatHostPort = tomcatHostPort;
+    }
+}
---
+++ monitor-console/monitor-web/src/main/java/com/creditease/monitor/service/MonitorTaskEtcdService.java
@@ -0,0 +1,110 @@
+package com.creditease.monitor.service;
+
+import com.alibaba.fastjson.JSON;
+import com.creditease.monitor.etcd.entity.MonitorNoteDataEntity;
+import com.creditease.monitor.mybatis.sqllite.grafana.po.MonitorTask;
+import mousio.etcd4j.EtcdClient;
+import mousio.etcd4j.promises.EtcdResponsePromise;
+import mousio.etcd4j.responses.EtcdException;
+import mousio.etcd4j.responses.EtcdKeysResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+/**
+ * 监控任务针对ETCD的操作
+ */
+@Service
+public class MonitorTaskEtcdService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MonitorTaskEtcdService.class);
+
+    private static final String separator = ",";
+
+    @Autowired
+    private EtcdClient etcdClient;
+
+    public static final String monitorTaskDir  = "/monitor";
+
+    public boolean upSert(MonitorTask monitorTask){
+        if(monitorTask != null){
+            String key = checkAndGetEtcdKey(monitorTask.getTaskName());
+            String cutTemplate = monitorTask.getCutTemplate();
+            String dataSourceServerIp =  monitorTask.getDataSourceServerIp();
+            String dataSourceLog = monitorTask.getDataSourceLog();
+            checkNull(cutTemplate);
+            checkNull(dataSourceServerIp);
+            checkNull(dataSourceLog);
+            MonitorNoteDataEntity noteDataEntity = new MonitorNoteDataEntity();
+            noteDataEntity.setType("log");
+            noteDataEntity.setCleanRule(cutTemplate);
+            String[] pathArr = dataSourceLog.split(separator);
+            noteDataEntity.setPath(Arrays.asList(pathArr));
+            String[] dataSourceServerIpArr = dataSourceServerIp.split(separator);
+            noteDataEntity.setIpAddress(Arrays.asList(dataSourceServerIpArr));
+            String tomcatServerHost = monitorTask.getTomcatServerHost();
+            if(StringUtils.isNotBlank(tomcatServerHost)){
+                noteDataEntity.setTomcatHostPort(Arrays.asList(tomcatServerHost.split(separator)));
+            }
+            try {
+                String str = JSON.toJSONString(noteDataEntity);
+                EtcdResponsePromise<EtcdKeysResponse> responsePromise = etcdClient.put(key,str).send();
+                responsePromise.get();
+                return true;
+            }catch (Exception e){
+                logger.error("upSert error monitorTask={},errorMsg",JSON.toJSONString(noteDataEntity),e.getMessage());
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(String monitorTaskName){
+        String key = checkAndGetEtcdKey(monitorTaskName);
+        try {
+            EtcdResponsePromise<EtcdKeysResponse> responsePromise = etcdClient.delete(key).send();
+            responsePromise.get();
+            return true;
+        }catch (Exception e){
+            logger.info("delete key={} error,msg={}",key,e.getMessage());
+            return false;
+        }
+    }
+
+    public MonitorNoteDataEntity select(String monitorTaskName) throws Exception {
+        if(StringUtils.isNotBlank(monitorTaskName)){
+            String key = checkAndGetEtcdKey(monitorTaskName);
+            try {
+                EtcdResponsePromise<EtcdKeysResponse> responsePromise = etcdClient.get(key).send();
+                EtcdKeysResponse response = responsePromise.get();
+                String nodeValue = response.getNode().getValue();
+                if(StringUtils.isNotBlank(nodeValue)){
+                    return JSON.parseObject(nodeValue,MonitorNoteDataEntity.class);
+                }
+            }catch (EtcdException e){
+                return null;
+            }
+        }
+        return  null;
+    }
+
+    private void checkNull(String str){
+        if(StringUtils.isBlank(str)){
+            throw new NullPointerException();
+        }
+    }
+
+    private String checkAndGetEtcdKey(String key){
+        checkNull(key);
+        StringBuffer buffer = new StringBuffer(monitorTaskDir);
+        if(!key.startsWith("/")){
+            buffer.append("/");
+        }
+        buffer.append(key);
+        return buffer.toString();
+    }
+}
