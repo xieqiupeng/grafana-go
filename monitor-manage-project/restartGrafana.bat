b0b0f6db86afa8d0dab2612d174abc7be8dd200a
zhixinsong2 <zhixinsong2@creditease.cn>
2018-01-03 11:46:13 +0800

    初始化

---
+++ monitor-manage-project/restartGrafana.bat
@@ -0,0 +1,5 @@
+taskkill /f /im "grafana-server.exe"
+@cd "C:\Users\Administrator.20170712-151425\IdeaProjects\grafana462\bin"
+@start grafana-server.exe
+@cd "C:\Users\Administrator.20170712-151425\IdeaProjects\grafana462\data\plugins\monitor-manage-project"
+
