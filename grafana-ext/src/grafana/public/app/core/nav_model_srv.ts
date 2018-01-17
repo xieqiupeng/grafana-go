1bdd02cfbc44383a61607a6afb5b3e65438a8d72
xieqiupeng <xieqiupeng@gmail.com>
2018-01-17 14:55:59 +0800

    汉化界面

--- grafana-ext/src/grafana/public/app/core/nav_model_srv.ts
+++ grafana-ext/src/grafana/public/app/core/nav_model_srv.ts
@@ -150,33 +150,33 @@ export class NavModelSrv {
 
     if (dashboard.meta.canEdit) {
       menu.push({
-        title: 'Settings',
+        title: '设置',
         icon: 'fa fa-fw fa-cog',
         clickHandler: () => dashNavCtrl.openEditView('settings')
       });
 
       menu.push({
-        title: 'Templating',
+        title: '模板',
         icon: 'fa fa-fw fa-code',
         clickHandler: () => dashNavCtrl.openEditView('templating')
       });
 
       menu.push({
-        title: 'Annotations',
+        title: '标注',
         icon: 'fa fa-fw fa-comment',
         clickHandler: () => dashNavCtrl.openEditView('annotations')
       });
 
       if (!dashboard.meta.isHome) {
         menu.push({
-          title: 'Version history',
+          title: '查看历史',
           icon: 'fa fa-fw fa-history',
           clickHandler: () => dashNavCtrl.openEditView('history')
         });
       }
 
       menu.push({
-        title: 'View JSON',
+        title: '查看JSON',
         icon: 'fa fa-fw fa-eye',
         clickHandler: () => dashNavCtrl.viewJson()
       });
@@ -191,14 +191,14 @@ export class NavModelSrv {
     }
 
     menu.push({
-      title: 'Shortcuts',
+      title: '快捷键',
       icon: 'fa fa-fw fa-keyboard-o',
       clickHandler: () => dashNavCtrl.showHelpModal()
     });
 
     if (this.contextSrv.isEditor) {
       menu.push({
-        title: 'Save As ...',
+        title: '另存为 ...',
         icon: 'fa fa-fw fa-save',
         clickHandler: () => dashNavCtrl.saveDashboardAs()
       });
@@ -206,7 +206,7 @@ export class NavModelSrv {
 
     if (dashboard.meta.canSave) {
       menu.push({
-        title: 'Delete',
+        title: '删除',
         icon: 'fa fa-fw fa-trash',
         clickHandler: () => dashNavCtrl.deleteDashboard()
       });
